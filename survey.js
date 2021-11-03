1694292fa2c4a68d2b8e2852872abb6ee6e4386c
zinat <zinat.gholami@gmail.com>
2021-11-03 17:06:53 +0000

    new correction

--- survey.js
+++ survey.js
@@ -14,9 +14,13 @@ rl.question('what is your name? Nicknames are also acceptable :-) ', (answer1) =
     rl.question('what is an activity you like doing?  ', (answer2) => {
         outPut.activity = answer2
         rl.question('what do you listen to while doing that? ', (answer3) => {
+            outPut.listen = answer3
             rl.question('which meal is your favorite(eg: dinner, brunch,etc.) ', (answer4) => {
+                outPut.favourite = answer4
                 rl.question('what is your favourite thing to eat for that meal? ', (answer5) => {
+                    outPut.favouriteThing = answer5
                     rl.question('which sport is your absolute favourite?', (answer6) => {
+                        outPut.absoluteFavourite = answer6
         var result = (+answer1) + (+answer2) +(+answer3) +(+answer4)+ (+answer5) +(+answer6)
         console.log(outPut);
         rl.close();
